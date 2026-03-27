adad9bd97c6e6e7270fff2396b386b0670ac7d88
HaNhienNe <nihonsandesu@gmail.com>
2026-03-27 14:54:44 +0700

    Save current work before creating dev branch

---
+++ oms-be/src/test/java/com/sangdd/omsbe/OmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangdd.omsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
